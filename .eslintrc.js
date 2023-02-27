26870b446a3c1ee4dc521936d889d3be2605d4d8
Ericlys Moreira <ericlysm@gmail.com>
2023-02-27 00:47:55 -0300

    chore: app setup with eslint, prettier, styled-components, theme, fonts

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    '@typescript-eslint/no-empty-interface': 'off',
+    'react/jsx-props-no-spreading': 'error',
+    'react/prop-types': 'off',
+  },
+}
--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from '@screens/Home'
+import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans'
+import { ActivityIndicator } from 'react-native'
+import { ThemeProvider } from 'styled-components/native'
+import theme from '@theme/index'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [ fontsLoaded ] = useFonts({
+    NunitoSans_400Regular, NunitoSans_700Bold
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <ThemeProvider theme={theme}>
+      { fontsLoaded ? <Home/> : <ActivityIndicator /> }
+    </ThemeProvider>
+  )
+}
