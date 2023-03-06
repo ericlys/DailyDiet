bb56c3301b4338fa03aa491e384aaf1772d999e0
Ericlys Moreira <ericlysm@gmail.com>
2023-03-05 22:03:25 -0300

    feat: sorting list

--- src/utils/groupMealListByDate.ts
+++ src/utils/groupMealListByDate.ts
@@ -1,5 +1,10 @@
 import { MealStorage } from '@storage/meal/MealStorage'
 
+function convertToDatetime(dataString: string) {
+  const [day, month, year] = dataString.split('/')
+  return new Date(`${year}-${month}-${day}`).getTime()
+}
+
 export const groupMealListByDate = (meals: MealStorage[] ) => {
 
   const mealsGroupedByDate =  meals.reduce((acc, item) => {    
@@ -18,7 +23,7 @@ export const groupMealListByDate = (meals: MealStorage[] ) => {
         date,
         data: mealsGroupedByDate[date],
       }
-    })
+    }).sort((a, b) =>  convertToDatetime(b.date) - convertToDatetime(a.date))
 
   return result
 }
